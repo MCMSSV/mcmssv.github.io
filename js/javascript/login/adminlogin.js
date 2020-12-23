521a47b36cb160ac050888546f52feaee44c5596
MCMSSV <72972052+MCMSSV@users.noreply.github.com>
2020-12-23 15:12:49 +0900

    Create adminlogin.js

---
+++ js/javascript/login/adminlogin.js
@@ -0,0 +1,10 @@
+function checklogin(){
+				var v = grecaptcha.getResponse();
+				if (v.length ==0) {
+					alert ("'로봇이 아닙니다.'를 체크해주세요.");
+					return false;
+				} else {
+					location.reload();
+					alert(userid.value);
+				}
+			}
